c0f357e305da82d90e964daed649df01d5d7f777
David Kuhner <dav.kuhn@gmail.com>
2014-04-15 08:40:15 +0200

    Initial projects commit

---
+++ passwordChecker/passwordChecker/src/passwordchecker/PasswordChecker.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passwordchecker;
+
+/**
+ *
+ * @author david.kuhner
+ */
+public class PasswordChecker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello password checker");
+    }
+}
---
+++ passwordDictionary/passwordDictionary/src/passworddictionary/PasswordDictionary.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passworddictionary;
+
+/**
+ *
+ * @author david.kuhner
+ */
+public class PasswordDictionary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello password dictionary");
+    }
+}
